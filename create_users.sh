3a3c471f534dbef2e520b80d76e61c2b85aa17a1
Sami Eyassu Tesfay <samiyos043@gmail.com>
2026-05-16 18:14:19 +0200

    Refactor create_users.sh for improved readability

--- create_users.sh
+++ create_users.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
-  exit 1
-fi
-
-if [ "$#" -lt 1 ]; then
-  exit 1
-fi
-
 for user in "$@"; do
-
   if id "$user" >/dev/null 2>&1; then
     continue
   fi
 
   useradd -m -s /bin/bash "$user"
 
-  mkdir -p /home/$user/Documents
-  mkdir -p /home/$user/Downloads
-  mkdir -p /home/$user/Work
+  mkdir -p /home/$user/Documents /home/$user/Downloads /home/$user/Work
 
-  chmod 700 /home/$user/Documents
-  chmod 700 /home/$user/Downloads
-  chmod 700 /home/$user/Work
+  chmod 700 /home/$user/Documents /home/$user/Downloads /home/$user/Work
 
   chown -R $user:$user /home/$user
 
@@ -31,5 +18,4 @@ for user in "$@"; do
 
   chmod 644 /home/$user/welcome.txt
   chown $user:$user /home/$user/welcome.txt
-
 done
