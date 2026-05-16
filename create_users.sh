4fa1c6ccf64514128e78e8a0a01a5b8bf8b253f1
Sami Eyassu Tesfay <samiyos043@gmail.com>
2026-05-16 17:32:10 +0200

    Refactor user creation script for better user listing

--- create_users.sh
+++ create_users.sh
@@ -12,40 +12,35 @@ if [ "$#" -eq 0 ]; then
   exit 1
 fi
 
-# Hämta befintliga användare (endast riktiga användare)
-existing_users=$(ls /home)
+# Hämta befintliga systemanvändare
+existing_users=$(cut -d: -f1 /etc/passwd)
 
 # Loopa igenom alla användarnamn
 for username in "$@"; do
 
-  # Kontrollera om användaren redan finns
   if id "$username" &>/dev/null; then
     echo "Användaren $username finns redan."
     continue
   fi
 
-  # Skapa användare med hemkatalog
+  # Skapa användare
   useradd -m -s /bin/bash "$username"
-
   home_dir="/home/$username"
 
   # Skapa mappar
   mkdir -p "$home_dir/Documents" "$home_dir/Downloads" "$home_dir/Work"
 
-  # Sätt rättigheter (endast ägare)
+  # Rättigheter
   chmod 700 "$home_dir/Documents" "$home_dir/Downloads" "$home_dir/Work"
-
-  # Sätt ägarskap
   chown -R "$username:$username" "$home_dir"
 
   # Skapa welcome.txt
   {
-  echo "Välkommen $username"
-  cut -d: -f1 /etc/passwd
-} > "$home_dir/welcome.txt"
+    echo "Välkommen $username"
+    echo "$existing_users"
+  } > "$home_dir/welcome.txt"
 
-  # Rättigheter för welcome.txt
-  chmod 700 "$home_dir/welcome.txt"
+  chmod 600 "$home_dir/welcome.txt"
   chown "$username:$username" "$home_dir/welcome.txt"
 
 done
